7d879cb548d6f2fc40d7f6f2c9288cf5a73ac217
nicomoregu <nicomoregu@gmail.com>
2017-04-19 07:25:40 -0500

    first commit

---
+++ app/src/main/java/edu/eci/com/foreignmobile/NewTutorialActivity.java
@@ -0,0 +1,13 @@
+package edu.eci.com.foreignmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tutorial);
+    }
+}
