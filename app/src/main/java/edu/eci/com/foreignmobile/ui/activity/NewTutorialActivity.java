9b6e4eff5f91695e92a0502d58b40ba7549c8bf3
tatisandreea <tata_higuera@hotmail.com>
2017-04-25 09:26:31 -0500

    formulario para una nueva tutoria

--- app/src/main/java/edu/eci/com/foreignmobile/ui/activity/NewTutorialActivity.java
+++ app/src/main/java/edu/eci/com/foreignmobile/ui/activity/NewTutorialActivity.java
@@ -12,14 +12,24 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.eci.com.foreignmobile.R;
 
 public class NewTutorialActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
+    Spinner listLanguages;
+    String[] languages = {"¿Qué idioma quieres practicar?","English", "Español", "Portugues", "Deutsch"};
+    CalendarView calendarView;
+    TextView dateDisplay;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,66 @@ public class NewTutorialActivity extends AppCompatActivity
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ///
+        selectLanguage();
+        selectDate();
+    }
+
+    private void selectDate() {
+
+        calendarView = (CalendarView) findViewById(R.id.calendar);
+        dateDisplay = (TextView) findViewById(R.id.date_display);
+        dateDisplay.setText("Día seleccionado: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                dateDisplay.setText("Día seleccionado: " + i2 + " / " + i1 + " / " + i);
+
+                Toast.makeText(getApplicationContext(), "Día seleccionado:\n" + "Día = " + i2 + "\n" + "Mes = " + i1 + "\n" + "Año = " + i, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void selectLanguage() {
+        listLanguages = (Spinner) findViewById(R.id.list_languages);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+        listLanguages.setAdapter(adapter);
+
+        listLanguages.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast;
+                switch (position){
+                    case 1:
+                        toast = Toast.makeText(getApplicationContext(), languages[position], Toast.LENGTH_LONG);
+                        toast.show();
+
+                        break;
+                    case 2:
+                        toast = Toast.makeText(getApplicationContext(), languages[position], Toast.LENGTH_LONG);
+                        toast.show();
+                        break;
+                    case 3:
+                        toast = Toast.makeText(getApplicationContext(), languages[position], Toast.LENGTH_LONG);
+                        toast.show();
+                        break;
+                    case 4:
+                        toast = Toast.makeText(getApplicationContext(), languages[position], Toast.LENGTH_LONG);
+                        toast.show();
+                        break;
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
@@ -52,13 +122,13 @@ public class NewTutorialActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_newTutorial:
-                    mTextMessage.setText(R.string.title_new_tutorial);
+                    //mTextMessage.setText(R.string.title_new_tutorial);
                     return true;
                 case R.id.navigation_historial:
-                    mTextMessage.setText(R.string.title_historial);
+                    //mTextMessage.setText(R.string.title_historial);
                     return true;
                 case R.id.navigation_myTutorial:
-                    mTextMessage.setText(R.string.title_myTutorial);
+                    //mTextMessage.setText(R.string.title_myTutorial);
                     return true;
 
             }
