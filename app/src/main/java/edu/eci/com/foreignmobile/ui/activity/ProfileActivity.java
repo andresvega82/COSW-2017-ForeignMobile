9f7a6cfb2aefe6fb1999ae139d7651dd693aab94
NicolasGP <brayan.guzman@mail.escuelaing.edu.co>
2017-05-03 09:31:43 -0500

    vista

--- app/src/main/java/edu/eci/com/foreignmobile/ui/activity/ProfileActivity.java
+++ app/src/main/java/edu/eci/com/foreignmobile/ui/activity/ProfileActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -28,6 +33,7 @@ import edu.eci.com.foreignmobile.entities.User;
 public class ProfileActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    Intent intent;
     private String userId="";
     private String emailUser="";
     private User u=new User();
@@ -38,9 +44,10 @@ public class ProfileActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_profile);
-        Intent intent = getIntent();
+        intent = getIntent();
         userId = intent.getStringExtra("UserId");
         emailUser=intent.getStringExtra("email");
+
         TextView nombre= (TextView) findViewById(R.id.nombre);
         TextView mail=(TextView) findViewById(R.id.correo);
         TextView age= (TextView) findViewById(R.id.edad);
@@ -57,14 +64,54 @@ public class ProfileActivity extends AppCompatActivity
         ///
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Perfil");
 
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.container);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        ///*/
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        ///
+
+        findViewById(R.id.activity_profile).setVisibility(View.VISIBLE);
+        findViewById(R.id.content_new_tutorial).setVisibility(View.INVISIBLE);
+        findViewById(R.id.activity_my_tutorials).setVisibility(View.INVISIBLE);
+        */
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent intent = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_newTutorial:
+                    intent = new Intent(ProfileActivity.this,NewTutorialActivity.class);
+                    intent.putExtra("userID",userId);
+                    intent.putExtra("view", "1");
+                    startActivity(intent);
+                case R.id.navigation_historial:
+                    intent = new Intent(ProfileActivity.this, HistorialActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_myTutorial:
+                    intent = new Intent(ProfileActivity.this, MyTutorialsActivity.class);
+                    startActivity(intent);
+                    return true;
+
+            }
+            return false;
+        }
+
+
+    };
 
     public void getClient(){
 
